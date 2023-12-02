7ff20e5550029ec36d4f938d23604304ee927405
wei <miguelxu124@.gmail.com>
2023-12-02 21:16:27 +0100

    advertencia

--- js/cita.js
+++ js/cita.js
@@ -1,3 +1,4 @@
+//la fecha seleccionada
 window.onload = function () { 
     Calendar.setup({
         inputField:"cita",
@@ -5,25 +6,40 @@ window.onload = function () {
         button:"selector",
     });
 }
-
+//función para comprobar la fecha elegida si es válida. 
 function fecha() { 
+    //advertencia de html
     var adv=document.getElementById("advertencia");
+    //conseguir la fecha seleccionada
     var cita=document.getElementById("cita").value;
-
+    var cita=new Date(cita);
+    var citaDay=cita.getDay();
+    //conseguir la fecha de hoy
     var hoy=new Date();
-    ano=hoy.getFullYear();
-    mes=hoy.getMonth();
-    dia=hoy.getDate();
+    var ano=hoy.getFullYear();
+    var mes=hoy.getMonth();
+    var dia=hoy.getDate();
+    //si el mes o día es menor de octubre, tendrá la formara como 0 mas mes o día
     mes=mes<10? '0'+mes:mes;
     dia=dia<10? '0'+dia:dia;
+    //lo deja como la forma de año-mes-día
     var hoyObj=ano+'-'+mes+'-'+dia;
-    if (cita>hoyObj) {
-        adv.innerHTML="fecha con válida : " +cita;
-        adv.style.color="green";
-        adv.style.display="block";
-    } else {
-        adv.innerHTML="fecha sin válida : "+cita;
+    if (cita < hoyObj) {
+    //si la fecha de cita es anterior al día de hoy, aparece "fecha no válida" en fuente rojo
+        adv.innerHTML="fecha no válida : "+cita;
         adv.style.color="brown";
         adv.style.display="block";
+    } else if(citaDay==0 || citaDay==6){
+        //si la fecha es el fin de semana, te advierte "elija un día laborable"
+        adv.innerHTML="Por favor, elija un día laborable";
+        adv.style.color="brown";
+        adv.style.display="block";
+    } else if (cita > hoy.setDate(hoy.getDate()+30)) {
+        //si la fecha es más tarde de un mes de hoy, te deja pedir otra vez una fecha como máximo 30 días en el futuro.
+        adv.innerHTML="Tan malo no estarás. Pide una fecha como máximo 30 días en el futuro";
+        adv.style.color="brown";
+        adv.style.display="block";
+    } else{
+        adv.style.display="none";
     }
 }
